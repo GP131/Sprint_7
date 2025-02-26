779ee58fd82992f5835668b5befd075ea2a0516d
Georgy Pomytkin <georgy.pomytkin@yandex.ru>
2025-02-26 20:38:59 +0100

    Separate urls file

---
+++ helpers/urls.py
@@ -0,0 +1,11 @@
+
+MAIN_URL = 'https://qa-scooter.praktikum-services.ru'
+
+CREATE_COURIER_URL = '/api/v1/courier'
+LOGIN_COURIER_URL = '/api/v1/courier/login'
+CREATE_ORDER_URL = '/api/v1/orders'
+GET_LIST_ORDERS_URL = '/api/v1/orders'
+ACCEPT_ORDER_URL = '/api/v1/orders/accept'
+GET_ORDER_BY_ID_URL = '/api/v1/orders/track'
+CANCEL_ORDER_URL = '/api/v1/orders/cancel'
+DELETE_COURIER_URL = "/api/v1/courier/"
